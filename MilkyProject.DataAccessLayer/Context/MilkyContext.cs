2376bc1dce1db907425505de43f964a9702d57f7
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-04-24 22:51:22 +0300

    Add project files.

---
+++ MilkyProject.DataAccessLayer/Context/MilkyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilkyProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkyProject.DataAccessLayer.Context
+{
+    public class MilkyContext : DbContext
+    {
+        protected override void OnConfiguring ( DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlServer ("Server=DESKTOP-6VB768D\\SQLEXPRESS02; Database=MilkyDb;Integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
---
+++ MilkyProject.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MilkyProject.BusinessLayer.Abstract;
+using MilkyProject.EntityLayer.Concrete;
+
+namespace MilkyProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController ( ICategoryService categoryService )
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var Values = _categoryService.TGetListAll(); 
+            return Ok(Values);
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            _categoryService.TInsert(category);
+            return Ok("category Başarıyla döndü");
+        }
+    }
+}
