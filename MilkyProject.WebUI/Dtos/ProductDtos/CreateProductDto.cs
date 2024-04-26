7de196cc1d10839aaba98179ba754cb584a76552
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-04-27 00:04:37 +0300

    #

---
+++ MilkyProject.WebUI/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MilkyProject.WebUI.Dtos.ProductDtos
+{
+    public class CreateProductDto
+    {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public Decimal oldPrice { get; set; }
+        public Decimal newPrice { get; set; }
+        public string imageUrl { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ MilkyProject.WebUI/Dtos/ProductDtos/UpdateProductDtos.cs
@@ -0,0 +1,14 @@
+﻿namespace MilkyProject.WebUI.Dtos.ProductDtos
+{
+    public class UpdateProductDtos
+    {
+            public int productId { get; set; }
+            public string productName { get; set; }
+            public Decimal oldPrice { get; set; }
+            public Decimal newPrice { get; set; }
+            public string imageUrl { get; set; }
+            public bool status { get; set; }
+        
+
+    }
+}
